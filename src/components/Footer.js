c8062da5c17776b4c2a1d5a849af7a586a5db8b2
rosel1z <cayennenovi@yandex.ru>
2025-04-09 21:42:28 +0300

    Первый коммит: загрузка проекта

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="page-footer teal lighten-1">
+            <div className="container">
+                © {new Date().getFullYear()} Новикова Елизавета 
+            </div>
+        </footer>
+    );
+}
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'bc04de82-72586ecb-11b725bb-07653935';
+export const API_URL_LIST = 'https://fortniteapi.io/v2/items/list?lang=en';
+export const API_URL_ITEM = 'https://fortniteapi.io/v2/items/get?lang=en';
+
